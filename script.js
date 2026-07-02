a3fad90a5337f02f3b46db0bda41e49d227cca39
ericperucho <ericperucho12@gmail.com>
2026-07-02 15:17:02 -0600

    scrip

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function randomColor(element){
-    const colors = ['greem','blue', 'red'];
-    const randomIndex = Math.floor(Math.random)
+// Function that applies one of 3 random colors: Green, Blue, or Red
+function randomColor(element) {
+    const colors = ['green', 'blue', 'red'];
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    element.style.color = colors[randomIndex];
 }
+
+// When someone clicks on any h5 tags, make them change to a random color
+const h5Tags = document.querySelectorAll('h5');
+
+h5Tags.forEach(function(h5) {
+    h5.addEventListener('click', function() {
+        randomColor(this);
+    });
+});
